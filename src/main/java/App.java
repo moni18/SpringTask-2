1a1078adfd71401d7cc6a60877f9940a36b0f182
Munira Turarova <muni.turaro@gmail.com>
2023-10-07 06:12:36 +0600

    '2.1.2'

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getName());
+
     }
 }
