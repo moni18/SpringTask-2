33d71a5a1382f9ba282471c14f384bce8b5fa4a2
Сергей <korkin.sergei@mail.ru>
2025-01-27 22:19:34 +0300

    решение спринг 2.1.2 - try 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanHW1 =
+                (HelloWorld) applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld beanHW2 =
+                (HelloWorld) applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat", Cat.class);
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat", Cat.class);
+
+        System.out.print("Результат сравнения HelloWorld: ");
+        System.out.println(beanHW1 == beanHW2);
+        System.out.print("Результат сравнения Cat: ");
+        System.out.println(beanCat1 == beanCat2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,10 @@
+import lombok.Data;
+
+@Data
+public class Cat {
+
+    private String name;
+    private int age;
+    private String color;
+
+}
